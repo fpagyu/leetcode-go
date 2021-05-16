a45be89c0b50323d9df71d007b4eafe3bbf84f5b
fpgayu <fpgayu@163.com>
2021-05-16 21:25:28 +0800

    #up

--- checkPossibility.go
+++ checkPossibility.go
@@ -2,19 +2,25 @@ package leetcode
 
 // https://leetcode-cn.com/problems/non-decreasing-array/
 
+// todo: 重刷
 func checkPossibility(nums []int) bool {
-	prev := nums[0]
-	cnt := 0
+	min := -100000
 
-	for i := 0; i < len(nums); i++ {
-		if nums[i] < prev {
-			nums[i] = prev
-			if cnt == 1 {
-				return false
+	cnt := 0
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] > nums[i+1] {
+			if min > nums[i+1] {
+				nums[i+1] = nums[i]
+			} else {
+				nums[i] = min
 			}
 			cnt++
 		}
-		prev = nums[i]
+
+		if cnt > 1 {
+			return false
+		}
+		min = nums[i]
 	}
 
 	return true
